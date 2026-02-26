7a99a1c62c8a2c8171f0d921778d2afde6eab59d
birakov <artembirakov@gmail.com>
2026-02-26 19:23:20 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -23,10 +23,6 @@ app.get("/json", (req, res) => {
 app.get("/json", (req, res) => {
   let message = "Hello json";
 
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-
   res.json({ message: message });
 });
 
