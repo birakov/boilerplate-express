ec9e281cb48887ade92e68aa1c9f87c78a607be4
birakov <artembirakov@gmail.com>
2026-02-26 19:09:15 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,15 +1,14 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+//console.log("Hello World");
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
-});
+//app.get('/', (req, res) => {
+//  res.send('Hello Express');
+//});
 
-app.get('/', (req, res) => {
-  const absolutePath = __dirname + '/views/index.html';
-  res.sendFile(absolutePath);
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
  module.exports = app;
