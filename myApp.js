9a20db070259f0034715567e148358b43813f5c2
birakov <artembirakov@gmail.com>
2026-02-26 19:21:03 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,7 @@
 let express = require('express');
+
+require('dotenv').config();
+
 let app = express();
 
 //console.log("Hello World");
@@ -15,6 +18,16 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   res.json({ "message": "Hello json" });
+});
+
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
 
  module.exports = app;
