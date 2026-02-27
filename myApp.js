c28df16d8e8e4132bca6dda9db573d8509ea72a5
birakov <artembirakov@gmail.com>
2026-02-27 18:05:45 +0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,50 +5,47 @@ const bodyParser = require('body-parser');
 
 let app = express();
 
+// body-parser — САМЫЙ ПЕРВЫЙ middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Логгер запросов
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+// Статические файлы
 app.use("/public", express.static(__dirname + "/public"));
 
+// Главная страница
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// JSON с uppercase
 app.get("/json", (req, res) => {
   let message = "Hello json";
-
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
   }
-
   res.json({ message: message });
 });
 
+// Эхо
 app.get('/:word/echo', (req, res) => {
-  const word = req.params.word;
-  res.json({ echo: word });
+  res.json({ echo: req.params.word });
 });
 
+// Временной сервер
 app.get('/now',
-  function(req, res, next) {
+  (req, res, next) => {
     req.time = new Date().toString();
     next();
   },
-  function(req, res) {
-    res.json({ time: req.time });
-  }
+  (req, res) => res.json({ time: req.time })
 );
 
-//app.get("/name", (req, res) => {
-//  const first = req.query.first;
-//  const last  = req.query.last;
-// res.json({ name: `${first} ${last}` });
-//});
-
+// ← ТОЛЬКО POST /name (GET-версия удалена!)
 app.post("/name", (req, res) => {
   const first = req.body.first;
   const last  = req.body.last;
